6ebd1c5fa56c217ff23ef97539592651cfce0eff
Tom Bell <tom.bell.code@gmail.com>
2013-07-12 21:04:20 +0000

    Modified web.js to read and send the contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+
+// Send the contents of index.html assuming utf8 encoding
+  var infile = "index.html";
+  var buffer = fs.readFileSync(infile);
+  var message = buffer.toString('utf8');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
